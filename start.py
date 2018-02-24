feaa40faa9d141dc448d1804fc8cef4ac0b59175
hexphase <13786214+hexphase@users.noreply.github.com>
2018-02-24 16:43:44 -0700

    start of work with NLTK

--- start.py
+++ start.py
@@ -1,6 +1,21 @@
 from flask import Flask
+import nltk
+
 app = Flask(__name__)
 
-@app.route("/")
-def welcome():
-    return "- Welcome to TrendLore.org -"
+sentence = "I love to eat spicy tacos, sip on coronas, all while relaxing at the beach."
+
+@app.route('/')
+def index():
+    return 'Index Page'
+
+@app.route('/tokenize')
+def tokenize():
+    tokens = nltk.word_tokenize(sentence)
+    return str(tokens)
+
+@app.route('/tag')
+def tag():
+    tokens = nltk.word_tokenize(sentence)
+    tags = nltk.pos_tag(tokens)
+    return str(tags)
