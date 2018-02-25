2e41c89972aea5903ca0cc8382254c2e745a5139
sal <sal2993@hotmail.com>
2018-02-24 20:41:15 -0700

    Updating my branch

--- start.py
+++ start.py
@@ -3,7 +3,7 @@ import nltk
 import sys
 sys.path.insert(0, '../')
 import keys
-keys.main() // Adds env variables for the API keys.
+keys.main() # Adds env variables for the API keys.
 
 app = Flask(__name__)
 
@@ -12,11 +12,22 @@ sentence = "I love to eat spicy tacos, sip on coronas, all while relaxing at the
 @app.route('/', methods=["GET", "POST"])
 def index():
     errors = []
-    results = {}
     if request.method == "POST":
       try:
         url = request.form['url']
-        r = requests.get(url)
+        req = requests.get(url)
+        
+        # Setting up API Keys and Secrets
+        apiConsumerKey = str(os.environ['CONSUMER_KEY'])
+        apiConsumerSecret = str(os.environ['CONSUMER_SECRET'])
+        apiAccessTokenKey= str(os.environ['ACCESS_TOKEN_KEY'])
+        apiAccessTokenSecret = str(os.environ['ACCESS_TOKEN_SECRET'])
+
+        # Get Request from Twitter API
+        api = TwitterAPI(apiConsumerKey, apiConsumerSecret, apiAccessTokenKey,
+				 apiAccessTokenSecret)
+        results = api.request('search/tweets', {'q':'%23' + str(req)})
+
       except:
         errors.append(
           " Couldnt get request"
