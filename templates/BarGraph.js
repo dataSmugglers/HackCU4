50d0e1e22ab0c52621a2cf236bae8f1add190aec
BOb <dlee67@msudenver.edu>
2018-02-25 02:24:25 -0700

    Graph starts from y=0.

--- templates/BarGraph.js
+++ templates/BarGraph.js
@@ -1,12 +1,26 @@
-var data; // Fetch data to here.
+/*
+  Please notice that 10 is being associated with lulz and 12 is being
+  associated with lol.
+*/
+/*integer*/var frequencyOfWord = [10, 12]; // y-axis element
+/*list*/var words = ["lulz", "lol"]; //X-axis element
 
 var chartInit = document.getElementById("myChart").getContext('2d');
 var chart = new Chart(chartInit, {
   type: 'bar',
   data: {
-    labels: ["TestingOne", "TestingTwo"],
+    labels: words,
     datasets:[{
-      data: [/* Pass data variable here. */]
-    }]
+      data: frequencyOfWord
+    }],
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
   }
 });
